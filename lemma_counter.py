4ee24a4540bf0ad3f023b3be412ecb369137f75d
onyxfox <deer95@ya.ru>
2018-07-15 23:39:24 +0300

    created lemma_counter.py chosen texts, written in sampleRU.txt statistics in overview.txt

---
+++ lemma_counter.py
@@ -0,0 +1,35 @@
+import pymystem3
+from collections import Counter
+
+def stemming(text):
+    lemmas = stemmer.lemmatize(text, speedup=True)
+    lemma_stat = Counter(lemmas).most_common()
+    for tuple in lemma_stat:
+        print('{}: {}'.format(tuple[1], tuple[0]))
+    print('\n#####\n')
+    over_file.write('Number of tokens: {}\n'.format(len(lemmas)))
+    over_file.write('Number of lemmas: {}\n\n'.format(len(lemma_stat)))
+
+
+texts_file = open('SampleRU.txt', 'r', encoding='utf-8')
+over_file = open('overview.txt', 'w', encoding='utf-8')
+
+stemmer = pymystem3.Mystem(disambiguation=False, entire_input=False)
+new_text = True
+text_str = ''
+
+for line in texts_file:
+    if line == '####\n':
+        stemming(text_str)
+        new_text = True
+        text_str = ''
+        continue
+    elif new_text:
+        over_file.write(line.upper())
+        new_text = False
+    else:
+        text_str += line
+stemming(text_str)
+
+texts_file.close()
+over_file.close()
